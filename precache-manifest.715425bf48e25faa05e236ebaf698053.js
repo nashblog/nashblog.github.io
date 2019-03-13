a387e1539319ed5fa8703d1dcc0c44239296e05e
AHTOH874 <melhov.tosha@gmail.com>
2019-03-13 17:06:47 +0300

    firtsPush

---
+++ precache-manifest.715425bf48e25faa05e236ebaf698053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d80a6e94b9846e2fc72740722a099a1",
+    "url": "/static/media/swiss721bt-italic.1d80a6e9.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bbb49bf1adbaa23530f0",
+    "url": "/static/js/main.14875445.chunk.js"
+  },
+  {
+    "revision": "a875eafc0c6f290e87e2",
+    "url": "/static/js/2.88529168.chunk.js"
+  },
+  {
+    "revision": "bbb49bf1adbaa23530f0",
+    "url": "/static/css/main.1ce3376c.chunk.css"
+  },
+  {
+    "revision": "a875eafc0c6f290e87e2",
+    "url": "/static/css/2.a08b37e2.chunk.css"
+  },
+  {
+    "revision": "69309571494d8587476d669656090400",
+    "url": "/index.html"
+  }
+];
